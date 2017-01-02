8ad1dac8fe93a743c0776f8e527ef666d7604d09
Valentín Antonio Bautista <vantonio@grupo.multimedios.com>
2017-01-02 14:06:56 -0600

    modulo canvas

---
+++ app/code/local/Fabric/Canvas/controllers/IndexController.php
@@ -0,0 +1,11 @@
+
+<?php
+
+class Fabric_Canvas_IndexController extends Mage_Adminhtml_Controller_Action {
+
+		public function indexAction() {
+				$this->loadLayout();
+				$this->renderLayout();
+		  }
+
+}
